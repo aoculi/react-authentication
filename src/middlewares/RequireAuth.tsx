2e2d4b971e423e2a9c191c4f799ff2ffe945f692
Alex <alexis.oculi@gmail.com>
2023-12-16 12:24:29 +0100

    fix: return null in middlewares when no loader provided

--- src/middlewares/RequireAuth.tsx
+++ src/middlewares/RequireAuth.tsx
@@ -8,7 +8,7 @@ export function RequireAuth({ redirectPath, loader }: RequireAuthProps) {
   const location = useLocation()
   const { isLoading, isAuthenticated } = useContext(AuthContext)
 
-  if (isLoading) return loader ? <>{loader}</> : <div>Loading...</div>
+  if (isLoading) return loader ? <>{loader}</> : null
   if (!isAuthenticated)
     return <Navigate to={redirectPath} state={{ from: location }} replace />
 
@@ -18,7 +18,7 @@ export function RequireAuth({ redirectPath, loader }: RequireAuthProps) {
 export function RequireNoAuth({ redirectPath, loader }: RequireAuthProps) {
   const { isLoading, isAuthenticated } = useContext(AuthContext)
 
-  if (isLoading) return loader ? <>{loader}</> : <div>Loading...</div>
+  if (isLoading) return loader ? <>{loader}</> : null
   if (!isAuthenticated) return <Outlet />
 
   return <Navigate to={redirectPath} replace />
