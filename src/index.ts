53077f56a02ca77c71b27f36542d67cd65549564
Alex <alexis.oculi@gmail.com>
2023-12-17 02:48:19 +0100

    fix: bad path

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
 export { AuthenticationProvider } from './providers/AuthenticationProvider'
 export { useAuthentication } from './hooks/useAuthentication'
 export * from './middlewares/RequireAuth'
-export * from './middlewares/RequireRoles'
+export * from './middlewares/RequirePermissions.js'
