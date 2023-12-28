2140d59781a8502487805ad6a6150a3e26f77dfc
Alex <alexis.oculi@gmail.com>
2023-12-28 10:50:48 +0100

    feat: new empty react example files

---
+++ example/src/index.jsx
@@ -0,0 +1,10 @@
+/* eslint-disable jsx-a11y/anchor-is-valid */
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+
+function App() {
+  return <div></div>
+}
+
+const rootElement = document.getElementById('root')
+ReactDOM.createRoot(rootElement).render(<App />)
